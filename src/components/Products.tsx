f3d8207deb0f58cccf723330c2c4455f83d01890
Lakshan <lakshanjayalath171@gmail.com>
2026-08-29 15:39:22 +0530

    Created product explorer component and finalize the Products section

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -4,6 +4,7 @@ import { ChevronLeft,ChevronRight } from 'lucide-react';
 
 const Products = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isOpen,setIsOpen] = useState(false);
 
   const handlePrevious = () => {
     setCurrentIndex((prevIndex) =>
@@ -20,7 +21,7 @@ const Products = () => {
   const currentProduct = products[currentIndex];
 
   return (
-    <div className="w-screen aspect-video mango-bg-dark-section px-10 py-10 flex items-center justify-center text-center">
+    <div className="w-screen aspect-video mango-bg-dark-section px-10 py-10 flex items-center justify-center text-center relative">
       <div>
         <div>
           <h1 className="mango-heading-white text-6xl">THE COLLECTION</h1>
@@ -46,13 +47,29 @@ const Products = () => {
                 alt={currentProduct.title}
                 className="w-24 h-72 object-cover transition-all duration-500"
               />
-              <button className='my-6 mango-btn-gradient'>Explore Product</button>
+              <button className='my-6 mango-btn-gradient' onClick={() => setIsOpen(true)}>
+                Explore Product
+              </button>
             </div>
 
             <button onClick={handleNext} className="mango-btn-outline"><ChevronRight /></button>
           </div>
         </div>
       </div>
+
+      {isOpen && (<div className="absolute top-0 left-0 bg-black/80 w-screen h-full aspect-video flex items-center justify-center" onClick={()=>setIsOpen(false)}>
+
+      <div className='apple px-10 py-6 flex items-center justify-center gap-3'>
+        <div className='flex-1 '>
+            <img src={currentProduct.image} alt={currentProduct.title} className=" object-cover w-24 h-72" />
+        </div>
+        <div className='flex-3'>
+            <h1 className="mango-heading-gradient text-4xl">{currentProduct.title}</h1>
+            <p className="mango-text-white">{currentProduct.description}</p>
+        </div>
+      </div>
+      
+      </div>)}
     </div>
   );
 }
